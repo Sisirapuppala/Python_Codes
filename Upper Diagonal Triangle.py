8e76fecd92ccf37187bbe30dfd04b909905f2de1
Sisirapuppala <47064932+Sisirapuppala@users.noreply.github.com>
2021-04-05 22:45:40 +0530

    Create Upper Diagonal Triangle.py

---
+++ Upper Diagonal Triangle.py
@@ -0,0 +1,22 @@
+//
+*****
+.****
+..***
+...**
+....*
+//
+def solve(n):
+    # write your code here
+    i = n
+    while i >= 1:
+        j = n
+        while j > i:
+       
+            print('.',end="")
+            j -= 1
+        k = 1
+        while k <= i:
+            print('*', end='')
+            k += 1
+        print()
+        i -= 1
