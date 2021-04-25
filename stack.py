a685939493777939ffe59ada2e52f34b82b5c316
Sisirapuppala <47064932+Sisirapuppala@users.noreply.github.com>
2021-04-25 14:23:04 +0530

    Create stack.py

---
+++ stack.py
@@ -0,0 +1,77 @@
+class Stack:
+    def __init__(self,size=0):
+        self.data=[]
+        self.top=-1
+        self.size=size
+    def push(self,element):
+        self.data.append(element)
+        self.top=self.top+1
+    def pop(self):
+        popped_element=None
+        if(self.top>-1):
+            popped_element=self.data.pop()
+            self.top=self.top-1
+        return popped_element
+    def peek(self):
+        return self.data[self.top]
+    def isEmpty(self):
+        if len(self.data)==0:
+            return 1
+        else:
+            return 0
+    def isFull(self):
+        if len(self.data)==(self.size):
+            return 1
+        else:
+            return 0
+
+
+
+
+
+if __name__ == "__main__": 
+    
+    test_cases=int(input()) # number of test cases
+    size=int(input()) # size of Stack
+    stack=Stack(size) # creating new stack object
+    
+    while(test_cases>0): 
+        instruction=input().split()
+        val=0
+        if len(instruction)>1:
+            val=int(instruction[1])
+
+        instruction=int(instruction[0])
+        #####
+        # Instruction 1 means Push
+        # Instruction 2 means Pop
+        # Instruction 3 means Peek
+        # Instruction 4 means isEmpty
+        # Instruction 5 means isFull
+        #####
+
+        if(instruction==1):  
+            print(f'push:{val}')
+            stack.push(val)
+
+        elif (instruction==2):
+            print(f'pop:{stack.pop()}')
+
+        elif (instruction==3):            
+            print(f'peek:{stack.peek()}')
+
+        elif(instruction==4):
+            print(f'isEmpty:{stack.isEmpty()}')
+
+        elif(instruction==5):
+            print(f'isFull:{stack.isFull()}')
+
+        test_cases=test_cases-1 
+        
+ #Operations:
+
+push:       To add the elements into Stack
+pop:         To remove the element from Stack and return the element.
+peek:        To get the top element from the Stack and return the element.
+isEmpty:   returns 1 is stack is empty. Else returns 0
+isFull:       returns 1 is stack is full. Else returns 0
